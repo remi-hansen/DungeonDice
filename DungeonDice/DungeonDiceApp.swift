a329f9d3ef2df8059c2a90a714aebaafbd9d2ef7
Remi Hansen <hansenrd@bc.edu>
2022-10-02 15:26:03 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Remi Pacifico Hansen on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
